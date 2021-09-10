5ab4f43e7008fb371170a1a38eb430277aa830c0
archana-17 <lcs2019041@iiitl.ac.in>
2021-09-10 11:49:28 +0530

    added model and changed settings and url

--- event_management/club/models.py
+++ event_management/club/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.urls import reverse
+class Club(models.Model):
+    club_name = models.CharField(max_length = 255)
+    email = models.EmailField(max_length = 255)
+    password = models.CharField(max_length = 255)
+
+    def get_absolute_url(self):
+        return reverse("club:index")
+    
+    
